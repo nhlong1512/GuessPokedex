61baf126425efe20a6c16397aa7805b8b4f17a75
nhlong1512 <20521569@gm.uit.edu.vn>
2023-05-11 11:54:07 +0700

    Setup ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "antd";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello There
+      <h1 className="text-3xl text-red-600 font-bold underline">
+        Hello world!
+      </h1>
+      <Button type="dashed" className="bg-red-500">Dashed Button</Button>
     </div>
   );
 }
