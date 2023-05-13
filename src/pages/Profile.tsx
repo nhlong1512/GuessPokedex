6d11aed802496459a68dc71c72e78683c1e29150
nhlong1512 <20521569@gm.uit.edu.vn>
2023-05-13 11:42:23 +0700

    push

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,16 +1,71 @@
-import { Button, Col, ConfigProvider, Divider, Input, Row } from "antd";
+import {
+  Button,
+  Col,
+  ConfigProvider,
+  Divider,
+  Input,
+  Row,
+  Upload,
+  message,
+} from "antd";
 import { log } from "console";
-import React from "react";
+import React, { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../app/hook";
 import thumbnail from "../assets/images/Thumbnail.svg";
 import userImg from "../assets/images/user.png";
 import { User } from "../model/model";
+import type { UploadChangeParam } from "antd/es/upload";
+import type { RcFile, UploadFile, UploadProps } from "antd/es/upload/interface";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+
+const getBase64 = (img: RcFile, callback: (url: string) => void) => {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result as string));
+  reader.readAsDataURL(img);
+};
+
+const beforeUpload = (file: RcFile) => {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!");
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
+};
 
 const Profile = () => {
   const dispatch = useAppDispatch();
-  const user:User | null = useAppSelector(state => state.user.currentUser)
-  console.log(user);
-  
+  const user: User | null = useAppSelector((state) => state.user.currentUser);
+
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState<string>();
+
+  const handleChange: UploadProps["onChange"] = (
+    info: UploadChangeParam<UploadFile>
+  ) => {
+    if (info.file.status === "uploading") {
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === "done") {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj as RcFile, (url) => {
+        setLoading(false);
+        setImageUrl(url);
+      });
+    }
+  };
+
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
+
   return (
     <Col span={24} className="bg-[#fff] py-[48px]">
       <ConfigProvider
@@ -23,6 +78,21 @@ const Profile = () => {
         <Col span={18} offset={3} className="mt-[40px] mb-[40px]">
           <Row className="mt-[40px] flex items-center justify-between">
             <div className="flex items-center gap-[40px]">
+              {/* <Upload
+                name="avatar"
+                listType="picture-circle"
+                className="avatar-uploader"
+                showUploadList={false}
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                beforeUpload={beforeUpload}
+                onChange={handleChange}
+              >
+                {imageUrl ? (
+                  <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+                ) : (
+                  uploadButton
+                )}
+              </Upload> */}
               <img
                 src={user?.photoURL ? user.photoURL : userImg}
                 alt="userImg"
@@ -30,7 +100,7 @@ const Profile = () => {
                 style={{ boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px" }}
               />
               <div>
-                <h3 className="text-[24px] my-0">Profile</h3>
+                <h3 className="text-[28px] my-0">Profile</h3>
                 <p className="my-0">Update your photo and personal details.</p>
               </div>
             </div>
@@ -52,7 +122,7 @@ const Profile = () => {
                   placeholder="Full Name"
                   type="text"
                   name="fullName"
-                  value={user?.fullName? user.fullName : ''}
+                  value={user?.fullName ? user.fullName : ""}
                   // onChange={handleChange}
                   className="font-[500] px-[8px] py-[10px] rounded-[4px] min-w-[300px]"
                 />
@@ -69,7 +139,7 @@ const Profile = () => {
                   placeholder="Your email"
                   type="text"
                   name="email"
-                  value={user?.email ? user.email : ''}
+                  value={user?.email ? user.email : ""}
                   disabled
                   // onChange={handleChange}
                   className="font-[500] px-[8px] py-[10px] rounded-[4px] min-w-[300px]"
@@ -87,7 +157,7 @@ const Profile = () => {
                   placeholder="Your phone"
                   type="number"
                   name="phone"
-                  value = {user?.phoneNumber ? user.phoneNumber : ''}
+                  value={user?.phoneNumber ? user.phoneNumber : ""}
                   // onChange={handleChange}
                   className="font-[500] px-[8px] py-[10px] rounded-[4px] min-w-[300px]"
                 />
